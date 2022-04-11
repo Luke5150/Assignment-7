fbf0e97f3cbef9b1c6370081b668c1835bf59f80
Luke5150 <lhensley@mail.bradley.edu>
2022-04-10 19:00:36 -0500

    Prototype 4

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed;
+
+    private float forwardInput;
+
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    private float powerupStrength = 15f;
+
+    public GameObject powerupIndicator;
+
+    public Text loseText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("Focalpoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -.05f, 0);
+
+        if(gameObject.transform.position.y < -10)
+        {
+            loseText.gameObject.SetActive(true);
+            Time.timeScale = 0f;
+
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("Prototype 4 Finished");
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(powerupCountdown());
+        }
+    }
+
+    IEnumerator powerupCountdown()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 awayFromPLayer = collision.gameObject.transform.position.normalized;
+
+            enemyRb.AddForce(awayFromPLayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    public int enemyCount = 1;
+    public int waveNumber = 1;
+
+    public GameObject powerupPrefab;
+
+    public Text waveText;
+    public Text winText;
+
+    public bool hasWon = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       SpawnEnemyWave(waveNumber);
+       SpawnPowerup(1);
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length; 
+
+        if(Input.GetKeyDown(KeyCode.R) && hasWon)
+        {
+             Time.timeScale = 1f;
+             SceneManager.LoadScene("Prototype 4 Finished");
+        }
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            waveText.text = "Current Wave: " + waveNumber;
+            if(waveNumber > 10)
+            {
+                Time.timeScale = 0f;
+                winText.gameObject.SetActive(true);
+                hasWon = true;
+                
+            }
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup(1);
+        }
+    }
+
+    private void SpawnPowerup(int powerupsToSpawn)
+    {
+        for (int i = 0; i < powerupsToSpawn; i++)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+}
